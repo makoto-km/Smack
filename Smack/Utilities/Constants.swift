6bca1cb8d5b2eb91606475db350047a988509a4e
makoto <km_makoto@outlook.jp>
2017-10-14 22:10:43 +0900

    2017/10/14 Lesson9

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,34 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://smackchattychatchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+//let URL_LOGIN = "\(BASE_URL)account/login"
+//let URL_USER_ADD = "\(BASE_URL)user/add"
+//let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
+//let URL_GET_CHANNELS = "\(BASE_URL)channel/"
+//let URL_GET_MESSAGES = "\(BASE_URL)message/byChannel/"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+//let BEARER_HEADER = [
+//    "Authorization":"Bearer \(AuthService.instance.authToken)",
+//    "Content-Type": "application/json; charset=utf-8"
+//]
+
