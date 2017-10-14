e96f879792910effb0c3b82a1f88c4d890f4910e
makoto <km_makoto@outlook.jp>
2017-10-14 22:39:46 +0900

    2017/10/14 Lesson10

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +27,27 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+        
     }
     
 }
---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Makoto Kishina on 2017/10/14.
+//  Copyright © 2017年 Makoto Kishina. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
