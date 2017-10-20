d0e32f061efd651fa340062c52e12b9fe974efb8
makoto <km_makoto@outlook.jp>
2017-10-20 22:18:26 +0900

    2017/10/20 Lesson24

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Makoto Kishina on 2017/10/20.
+//  Copyright © 2017年 Makoto Kishina. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
